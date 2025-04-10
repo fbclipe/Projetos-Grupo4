5fef73b69a237f7632b67d0fccdde14d7c9ec97f
iduclerc <142417716+iduclerc@users.noreply.github.com>
2025-04-10 09:36:48 -0300

    Front RegisterUser

--- projetos4/client/src/App.js
+++ projetos4/client/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import RegisterUser from './components/RegisterUser'; // Importe o componente de cadastro
 
 function App() {
   return (
@@ -18,6 +19,11 @@ function App() {
           Learn React
         </a>
       </header>
+
+      {/* Renderize aqui o componente RegisterUser */}
+      <main>
+        <RegisterUser />
+      </main>
     </div>
   );
 }
