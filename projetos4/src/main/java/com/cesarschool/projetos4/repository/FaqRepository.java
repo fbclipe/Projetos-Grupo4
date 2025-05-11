75eed7d8fef45702160d3e5c9243be8f344897f2
Lucas Calabria <lvcalabria@gmail.com>
2025-05-11 20:43:27 -0300

    historia de FAQ
    .

---
+++ projetos4/src/main/java/com/cesarschool/projetos4/repository/FaqRepository.java
@@ -0,0 +1,26 @@
+package com.cesarschool.projetos4.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cesarschool.projetos4.entity.Faq;
+import com.cesarschool.projetos4.entity.User;
+
+@Repository
+public interface FaqRepository extends JpaRepository<Faq, Long>  {
+    
+    // Buscar todas as respostas de uma pergunta (baseada no ID da pergunta)
+    List<Faq> findByPerguntaPai(Faq perguntaPai);
+
+    // Buscar a pergunta original de uma resposta
+    Faq findByIdAndPerguntaPaiIsNull(Long id);
+
+    // Buscar todas as perguntas (respostas null)
+    List<Faq> findByPerguntaIsNotNull();
+
+    // Buscar todas as respostas para uma pergunta específica
+    List<Faq> findByPerguntaAndPerguntaPaiIsNotNull(String pergunta);
+}
